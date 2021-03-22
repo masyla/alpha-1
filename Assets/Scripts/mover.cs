3b50ad5744639e35bbc658da3caeaa77cbba5692
SuperSayem <35432821+SuperSayem@users.noreply.github.com>
2021-03-21 20:36:56 -0400

    Player Movement script improve
    The player controls does not invert anymore.

--- Assets/Scripts/mover.cs
+++ Assets/Scripts/mover.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class mover : MonoBehaviour
 {
-    [SerializeField] float movespeed = 8f;
+    [SerializeField] float speed = 8f;
+    [SerializeField] float fallSpeed = -2.5f;
+    Rigidbody rb;
+    //[SerializeField] public float ballSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(1, 0, 0); 
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * movespeed;
-        float zvalue = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
-        transform.Translate(xValue ,0 ,zvalue);  
+
+        float xMov = Input.GetAxis("Horizontal");
+        float zMov = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(xMov, fallSpeed, zMov) * speed;
+
+        //float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * movespeed;
+        //float zvalue = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
+        //transform.Translate(xValue, 0, zvalue);
+
+
     }
 }
